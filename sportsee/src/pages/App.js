6e2690a3ce6fe93fb94225eb52356e1992ddb8bb
pletine <pierre.letinevez@gmail.com>
2023-08-23 20:10:39 +0200

    Import the frameworks used
    SASS and FontAwesome
    Create Prettier and Eslint config files

--- sportsee/src/pages/App.js
+++ sportsee/src/pages/App.js
@@ -1,9 +1,11 @@
-import '../styles/App.css';
+import '../styles/App.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function App() {
   return (
     <div className="App">
       Hello Word
+      <FontAwesomeIcon icon="fa-duotone fa-lighthouse" />
     </div>
   );
 }
