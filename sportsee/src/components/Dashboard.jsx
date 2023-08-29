28f0abb4776de516a15c05397c63952690c396ef
pletine <pierre.letinevez@gmail.com>
2023-08-29 17:50:27 +0200

    Download all the data to print the different chars

--- sportsee/src/components/Dashboard.jsx
+++ sportsee/src/components/Dashboard.jsx
@@ -2,24 +2,46 @@ import '../styles/Dashboard.scss';
 import useFetch from '../scripts/useFetch';
 
 function Dashboard() {
+  const idUser = 12;
   const domain = 'http://localhost:3000';
-  const uriTest = '/user/12';
+  const mainURI = '/user/' + idUser.toString() + '/';
 
-  const { data, loading, error } = useFetch(domain + uriTest);
+  const {
+    // User Data
+    data: userData,
+    loading: loadUserData,
+    error: errorUserData,
+  } = useFetch(domain + mainURI);
+
+  const {
+    // User Activity
+    data: userActivity,
+    loading: loadUserActivity,
+    error: errorUserActivity,
+  } = useFetch(domain + mainURI + 'activity');
+
+  const {
+    // User Average Sessions
+    data: userAverageSessions,
+  } = useFetch(domain + mainURI + 'average-sessions');
+
+  const {
+    // User Performance
+    data: userPerformance,
+  } = useFetch(domain + mainURI + 'performance');
 
   return (
     <main>
       <article>
-        <h1>Bonjour Thomas</h1>
+        <div className="App">
+          {loadUserData && <h1>{loadUserData}</h1>}
+          {userData && <h1>Bonjour {userData.userInfos.firstName}</h1>}
+          {errorUserData && <h1>{errorUserData}</h1>}
+        </div>
         <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
         <div className="averageTime"></div>
         <div className="radar"></div>
         <div className="score"></div>
-        <div className="App">
-          {loading && <p>{loading}</p>}
-          {data && <p>{data.keyData.carbohydrateCount}</p>}
-          {error && <p>{error}</p>}
-        </div>
       </article>
 
       <aside>
