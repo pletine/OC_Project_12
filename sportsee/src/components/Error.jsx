d88a1cdb438c3609ded88a5e5eb26481a2fdecf7
pletine <pierre.letinevez@gmail.com>
2023-09-14 10:49:32 +0400

    Write the content of the error page

--- sportsee/src/components/Error.jsx
+++ sportsee/src/components/Error.jsx
@@ -1,15 +1,29 @@
 const stylePage = {
-  backgroundColor: 'black',
+  width: '100%',
+  lineHeight: '24px',
 };
 
 const styleH1 = {
-  color: 'white',
+  fontFamily: 'Roboto',
+  fontSize: '48px',
+  fontWeight: '500',
+  padding: '0.5em',
+};
+
+const styleP = {
+  fontFamily: 'Roboto',
+  fontSize: '18px',
+  fontWeight: '400',
+  padding: '0.2em 1.5em',
 };
 
 export default function Error() {
   return (
     <div style={stylePage}>
-      <h1 style={styleH1}>Error</h1>
+      <h1 style={styleH1}>Désolé, cette page n'existe pas</h1>
+      <p style={styleP}>
+        Veuillez essayer un autre identifiant ou une autre page
+      </p>
     </div>
   );
 }
